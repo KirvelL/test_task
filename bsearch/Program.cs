493b2579e3d595dd42179fc95820c2fa5e7012d2
GaMByP <pavel.kirvell@gmail.com>
2019-11-28 20:13:04 +0300

    Upload visual studio solution

---
+++ bsearch/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bsearch
+{
+    class Program
+    {
+        public int[] CreateArray()
+        {
+            Console.Write("Enter the number of numbers: ");
+            int NumOfNums = Int32.Parse(Console.ReadLine());
+            int[] array = new int[NumOfNums];
+            for (int i = 0; i < NumOfNums; i++)
+            {
+                bool exceptions = false;
+                while (!exceptions)
+                {
+                    try
+                    {
+                        Console.Write("Enter numbers: ");
+                        array[i] = Int32.Parse(Console.ReadLine());
+                        exceptions = true;
+
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine($"Error: You entered incorrect value ");
+                        exceptions = false;
+                        continue;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public int[] sorting(int[] array)
+        {
+            int temp;
+            for (int a = 0; a < array.Length-1; a++)
+            {
+                for (int i = a + 1; i < array.Length; i++)
+                {
+                    if (array[a] >= array[i])
+                    {
+                        temp = array[a];
+                        array[a] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public int bsearch(int[] array, int x = 0)
+        {
+            return array[0];
+        }
+        static void Main(string[] args)
+        {
+            int[] array;
+            int x;
+            Program ob = new Program();
+            array = ob.CreateArray();
+            array = ob.sorting(array);
+            int answer = ob.bsearch(array, x = 2);
+            if (answer <= x)
+            {
+                Console.WriteLine($"the first element of the array ({answer}) is less than or equal to x");
+            }
+            else if(answer > x)
+            {
+                Console.WriteLine($"Answer is {answer}");
+                Console.Write("Array values - ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
